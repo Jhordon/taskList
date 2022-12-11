3d0b42d05da7248f7cfbedad27c5acc14407b2d0
Jhordon <hankinsjordon@gmail.com>
2022-12-10 21:24:24 -0500

    Added mouseDown event to paragraph elements in itemList

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,29 @@
+import { useState } from "react";
+
 export const List = (props) => {
-    return(
-        <div className="list">
-            <h1> Tasks To Do: </h1>
-            {props.itemList.map((task, key) => {
-              return (
-                <div className="taskItem" key={key} style={{ backgroundColor: task.completed ? "#8DA077" : "FAF8F0" }}>
-                  <p> {task.task} </p>
-                  <button className="button" onClick={() => props.completeTask(task.id)}> Complete </button>
-                  <button className="button" onClick={() => props.deleteTask(task.id)}> Delete </button>
-                </div>
-              );
-            })}
+
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <div className="list">
+      <h1> Tasks To Do: </h1>
+      {props.itemList.map((task, key) => {
+        return (
+          <div className="taskItem"
+            key={key}
+            style={{ backgroundColor: task.completed ? "#8DA077" : "FAF8F0" }}
+          >
+            <p onMouseDown={() => setIsShown(!isShown)}
+            > {task.task} </p>
+            {isShown &&
+              <>
+                <button className="button" onClick={() => props.completeTask(task.id)}> Complete </button>
+                <button className="button" onClick={() => props.deleteTask(task.id)}> Delete </button>
+              </>
+            }
           </div>
-    );
+        );
+      })}
+    </div>
+  );
 }
